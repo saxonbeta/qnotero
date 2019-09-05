07d50834140c6be609876f452b91d6f19c02208a
Saxonbeta <saxonbeta@gmail.com>
2019-09-05 14:06:17 -0500

    Change logic for opening attachments. Only tested on Linux

--- libqnotero/qnoteroResults.py
+++ libqnotero/qnoteroResults.py
@@ -16,93 +16,100 @@ along with qnotero.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from libqnotero.qt.QtGui import QListWidget
-from libqnotero.qt.QtGui import QDrag
 from libqnotero.qt.QtCore import Qt, QUrl, QMimeData
 import shutil
 import tempfile
-import os.path
+import subprocess
+import os
+import platform
+
 
 class QnoteroResults(QListWidget):
+    """The Qnotero result list"""
 
-	"""The Qnotero result list"""
+    def __init__(self, qnotero):
 
-	def __init__(self, qnotero):
-
-		"""
+        """
 		Constructor
 
 		Arguments:
 		qnotero -- a Qnotero instance
 		"""
 
-		QListWidget.__init__(self, qnotero)
-		self.setMouseTracking(True)
+        QListWidget.__init__(self, qnotero)
+        self.setMouseTracking(True)
 
-	def mousePressEvent(self, e):
+    def mousePressEvent(self, e):
 
-		"""
+        """
 		Start a drag operation
 
 		Arguments:
 		e -- a QMouseEvent
 		"""
 
-		if e.button() == Qt.RightButton:
-			item = self.itemAt(e.pos())
-			if item == None:
-				return
-			note = item.zoteroItem.get_note()
-			if note != None:
-				self.qnotero.previewNote(note)
-			return
+        if e.button() == Qt.RightButton:
+            item = self.itemAt(e.pos())
+            if item == None:
+                return
+            note = item.zoteroItem.get_note()
+            if note != None:
+                self.qnotero.previewNote(note)
+            return
 
-		QListWidget.mousePressEvent(self, e)
-		qnoteroItem = self.currentItem()
-		if qnoteroItem == None:
-			return
-		if not hasattr(qnoteroItem, "zoteroItem"):
-			return
-		zoteroItem = qnoteroItem.zoteroItem
-		if zoteroItem.fulltext == None:
-			return
-		# Check the encodings of Zotero because this statement give errors
-		# path = zoteroItem.fulltext.encode("latin-1")
-		path = zoteroItem.fulltext
-		tmpName = '%s.pdf' % zoteroItem.filename_format()
-		tmpFile = os.path.join(tempfile.gettempdir(), tmpName)
-		suffix = 1
-		while os.path.exists(tmpFile):
-			tmpName = '%s-%d.pdf' % (zoteroItem.filename_format(), suffix)
-			tmpFile = os.path.join(tempfile.gettempdir(), tmpName)			
-			suffix += 1
-		try:
-			shutil.copy(path, tmpFile)
-		except:
-			print("qnoteroResults.mousePressEvent(): failed to copy file, sorry...")
-			return
-			
-		print("qnoteroResults.mousePressEvent(): prepare to copy %s" % path)
-		print("qnoteroResults.mousePressEvent(): prepare to copy (tmp) %s"
-			% tmpFile)
-		mimeData = QMimeData()
-		mimeData.setUrls([QUrl.fromLocalFile(tmpFile)])
-		mimeData.setData("text/plain", tmpFile)
-		drag = QDrag(self)
-		drag.setMimeData(mimeData)
-		drag.exec_(Qt.CopyAction)
+        QListWidget.mousePressEvent(self, e)
+        qnoteroItem = self.currentItem()
+        if qnoteroItem == None:
+            return
+        if not hasattr(qnoteroItem, "zoteroItem"):
+            return
+        zoteroItem = qnoteroItem.zoteroItem
+        if zoteroItem.fulltext == None:
+            return
+        # Check the encodings of Zotero because this statement give errors
+        # path = zoteroItem.fulltext.encode("latin-1")
+        path = zoteroItem.fulltext
+        tmpName = '%s.pdf' % zoteroItem.filename_format()
+        tmpFile = os.path.join(tempfile.gettempdir(), tmpName)
+        suffix = 1
+        while os.path.exists(tmpFile):
+            tmpName = '%s-%d.pdf' % (zoteroItem.filename_format(), suffix)
+            tmpFile = os.path.join(tempfile.gettempdir(), tmpName)
+            suffix += 1
+        try:
+            shutil.copy(path, tmpFile)
+        except:
+            print("qnoteroResults.mousePressEvent(): failed to copy file, sorry...")
+            return
 
-	def keyPressEvent(self, e):
+        print("qnoteroResults.mousePressEvent(): prepare to copy %s" % path)
+        print("qnoteroResults.mousePressEvent(): prepare to copy (tmp) %s"
+              % tmpFile)
 
-		"""
+        try:
+            if platform.system() == 'Darwin':  # macOS
+                subprocess.call(('open', tmpFile))
+            elif platform.system() == 'Windows':  # Windows
+                os.startfile(tmpFile)
+            else:  # linux variants
+                subprocess.call(('xdg-open', tmpFile))
+            print("qnoteroResults.mousePressEvent(): file opened")
+        except Exception as exc:
+            print("qnoteroResults.mousePressEvent(): failed to open file, sorry...")
+            print(exc)
+
+    def keyPressEvent(self, e):
+
+        """
 		Handle key presses
 
 		Arguments:
 		e -- a QKeyEvent
 		"""
 
-		if (e.key() == Qt.Key_Up and self.currentRow() == 0) \
-			or (e.key() == Qt.Key_F and Qt.ControlModifier & e.modifiers()):
-			self.qnotero.ui.lineEditQuery.selectAll()
-			self.qnotero.ui.lineEditQuery.setFocus()
-			return
-		QListWidget.keyPressEvent(self, e)
+        if (e.key() == Qt.Key_Up and self.currentRow() == 0) \
+                or (e.key() == Qt.Key_F and Qt.ControlModifier & e.modifiers()):
+            self.qnotero.ui.lineEditQuery.selectAll()
+            self.qnotero.ui.lineEditQuery.setFocus()
+            return
+        QListWidget.keyPressEvent(self, e)
