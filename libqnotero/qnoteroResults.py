f275a792c7b131d36229f354f90e6d0f618fa908
Saxonbeta <saxonbeta@gmail.com>
2019-09-07 22:42:47 -0500

    Correct filenames in windows

--- libqnotero/qnoteroResults.py
+++ libqnotero/qnoteroResults.py
@@ -76,7 +76,7 @@ class QnoteroResults(QListWidget):
             if platform.system() == 'Darwin':  # macOS
                 subprocess.call(('open', path))
             elif platform.system() == 'Windows':  # Windows
-                path = path.replace(u'\\', u'/')
+                path = os.path.normpath(path)
                 os.startfile(path)
             else:  # linux variants
                 subprocess.call(('xdg-open', path))
